7e363d4a14c23b284ef7297d31469602b5043b9d
rhpozzo@gmail.com <rhpozzo@gmail.com>
2019-05-18 19:20:09 -0400

    time and space complexity

--- bigOnotation.js
+++ bigOnotation.js
@@ -55,3 +55,43 @@ console.log(addUpto2(6));
   *  0(2n) NO!  just 0(n);
   *
   */
+
+/**Space Complexity
+ *  So far, we've been focusing on time complexity: How can we analyze the runtime of an algorithm as the size of the inputs increases?
+ *
+ * We can also use big O notation to analyze space complexity: How much additional memory do we need to allocate in order to run the code
+ * in our algorithm? Here we talk about auxilary space complexity when talking about space complexity.
+ *
+ *SPACE COMPLEXITY IN JS : RULES OF THUMB:
+    - Most primitives(booleans, numbers, undefined, null) are constant space.
+    - Strings require O(n) space (where n is the string length).
+    - Reference types are generally O(n), where n is the length (for arrays) or the number of keys (for objects);
+
+    Example of space complexity:
+ */
+function sum (arr){
+    let total = 0; //<<--- One number(space)
+    for(let i = 0; i < arr.length; i++){// we are not looking at time it takes to execute. let i = 0 inside is the only space that counts in this loop.
+        total += arr[i];
+    }
+    return total;
+}
+//What are the things that take up space, not time, in the previous example!? The two variables, let total and let i are the space in the algorithm
+/*arr can be 10, 50 or a 1000, it doesn't have an impact on space that's taken up because we only have these two variables and the exist no
+matter what. So what this really means is that we have constant pace, O(1) space! It's always the same, no matter the size of the input.*/
+
+//ANOTHER EXAMPLE:
+function doble(arr){
+    let newArr = [];//makes new arr;
+    for(let i = 0; i < arr.length; i++){
+        newArr.push(2*arr[i]);// multiples each item and makes newArr;
+    }
+    return newArr; //n numbers.
+}
+//**The space that's taken up is directly proportionate to N to input array. So n numbers we get 0(n) space!
+/**Big O RECAP:
+ *  To analyze the performance of an algorithm, we use Big O Notation.
+ *  Big O Notation can give us a high level understanding of the time or space complexity of an algorithm
+ *  Big O Notation doesn't care about precision, only about general trends(linear? quadratic? constant?)
+ *  The time or space complexity (as measured by Big O) depends only on the algorithm not the harware used to run the algorithm.
+ */
